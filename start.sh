d43a9eb4badf2a1567eb689916642347f7563b1c
fosteman <timfosteman@gmail.com>
2022-06-25 12:53:21 -0400

    installation script

--- start.sh
+++ start.sh
@@ -11,6 +11,20 @@ echo "|__/|__/\___/_.___/\____/_____/_/  /_/   "
 echo                          
 echo -e "\033[39m"
 
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+ sudo mkdir -p /etc/apt/keyrings
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
 almost_there(){
     echo 
     echo "===================="
